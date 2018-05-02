7ebde31cac0e01555cd813a053673a423ad4e375
deeem <karpov.dimon@gmail.com>
2018-05-02 21:34:09 +0300

    user_roles_table

---
+++ database/seeds/RolesSeeder.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $superadmin = Role::create([
+            'name' => 'Super Admin',
+            'slug' => 'superadmin',
+            'permissions' => [
+                'map' => true,
+                'dashboard' => true,
+                'charts' => true,
+                'components' => true,
+                'table' => true,
+            ]
+        ]);
+
+        $admin = Role::create([
+            'name' => 'Admin',
+            'slug' => 'admin',
+            'permissions' => [
+                'map' => true,
+                'dashboard' => true,
+                'charts' => true,
+                'components' => true,
+            ]
+        ]);
+
+        $manager = Role::create([
+            'name' => 'Manager',
+            'slug' => 'manager',
+            'permissions' => [
+                'tables' => true,
+            ]
+        ]);
+
+        $user = Role::create([
+            'name' => 'User',
+            'slug' => 'user',
+            'permissions' => [
+                'components' => true,
+            ]
+        ]);
+    }
+}
