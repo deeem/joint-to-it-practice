db48c51e7077afc27616995d8b7d097c7bc05afd
deeem <karpov.dimon@gmail.com>
2018-05-02 20:18:22 +0300

    login page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -46,12 +46,10 @@
             </div>
           </div>
         </div>
-        <div class="form-group row mb-0">
-            <div class="col-md-6 offset-md-4">
-                <button type="submit" class="btn btn-primary">
-                    {{ __('Register') }}
-                </button>
-            </div>
+        <div class="form-group">
+          <button type="submit" class="btn btn-primary btn-block">
+              {{ __('Register') }}
+          </button>
         </div>
       </form>
       <div class="text-center">
